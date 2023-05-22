f553ed31ad12abc3c501caebe79cd12f642828d9
dziwicz123 <michal.dziwisz@poczta.onet.pl>
2023-05-22 20:02:34 +0200

    17. Dodanie dokimentacji do klasy FifoRyhmer

--- src/main/java/edu/kis/vh/nursery/FifoRhymer.java
+++ src/main/java/edu/kis/vh/nursery/FifoRhymer.java
@@ -1,7 +1,12 @@
 package edu.kis.vh.nursery;
 
+/**
+ * FifoRhymer is a class taht extands DefaultCountingOutRhymer and overrides countOut method.
+ */
 public class FifoRhymer extends DefaultCountingOutRhymer {
-
+    /**
+     * temp is an instanc of a DefaultCountingOutRhymer class used in countOut method.
+     */
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
     
     @Override
